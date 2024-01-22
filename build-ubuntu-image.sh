7a748fce41760e5fe1d40d866dfaa7edd2818c5f
Sunnatillo <sunnat.samadov@est.tech>
2024-01-22 13:16:10 +0200

    fix dependency elements, improve build script
    Signed-off-by: Sunnatillo <sunnat.samadov@est.tech>

--- build-ubuntu-image.sh
+++ build-ubuntu-image.sh
@@ -9,4 +9,4 @@ export DIB_DEV_USER_AUTHORIZED_KEYS=${DIB_DEV_USER_AUTHORIZED_KEYS:-}
 
 export DIB_RELEASE=jammy 
 
-disk-image-create -a amd64 ubuntu-dev-env -o sunnat-test-3.qcow2
+disk-image-create -a amd64 base vm ubuntu-dev-env metal3-base devuser  -o sunnat-test-6.qcow2
